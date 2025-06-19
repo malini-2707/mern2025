e1a7ad16375034667db2cd98ae459886406c7f47
MALINI R <malinir.23it@kongu.edu>
2025-06-19 16:09:42 +0530

    Revert "html,css,react"

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,26 +1,20 @@
-// import Child from './component/Child'
-// import Home from './page/Home'
-// import About from './page/About'
-// import Skills from './page/Skills'
-// import Contact from './page/Contact'
-// import Login from './component/Login';
-import Counter from "./component/Counter"
-const App = () => {
-//   var fruits = ["Apple","Orange","Mango","Guava"];
-//   var users = {username:"Malini",password:"1927"}
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
   return (
     <>
     <div>
-    {/* <Child name='Malini' phnno='9876543210' dept='IT'/>
-    <Child name='Maluu' phnno='1234567890' dept='EIE'/>
-    <Home items={fruits} users={users}/>
-    <About />
-    <Skills />
-    <Contact />
-    <Login /> */}
-    <Counter />
+      <h1>
+        Hello! Malini
+      </h1>
     </div>
-      </>
+    <div>How are you?</div>
+    </>
   )
 }
 
