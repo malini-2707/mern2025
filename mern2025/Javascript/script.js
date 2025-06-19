6d31e5c6d904af37ad7741000cdf51bad03d7e1b
malini-2707 <malinir.23it@kongu.edu>
2025-06-19 16:30:44 +0530

    html,react

---
+++ mern2025/Javascript/script.js
@@ -0,0 +1,6 @@
+ var details = async()=>{
+            var response = await fetch('https://dummyjson.com/users');
+            var users = await response.json();
+            console.log(users)
+        }
+        details()
