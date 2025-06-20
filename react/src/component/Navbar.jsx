8d604b66f91c4dfcc5eff69475d643fdf63311de
malini-2707 <malinir.23it@kongu.edu>
2025-06-20 09:25:51 +0530

    react adder

---
+++ react/src/component/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Navbar.css'
+const Navbar = () => {
+  return (
+    <div>
+    <nav className='navbar'>
+        <li className='Link'>Home</li>
+        <li className='Link'>Skills</li>
+        <li className='Link'>Contact</li>
+        <li className='Link'>About</li>
+    </nav>
+    </div>
+  )
+}
+
+export default Navbar
