2c67a7fd97fc8362ffcefe96af26e582f4dd039e
malini-2707 <malinir.23it@kongu.edu>
2025-06-20 15:57:44 +0530

    react

--- Javascript/script.js
+++
@@ -1,6 +0,0 @@
- var details = async()=>{
-            var response = await fetch('https://dummyjson.com/users');
-            var users = await response.json();
-            console.log(users)
-        }
-        details()
